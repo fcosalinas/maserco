265c11167dc4cae40ec87a624cae8f5cbc9a99b3
Francisco Salinas <fcosalinas1987@gmail.com>
2012-12-28 11:22:09 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maserco::Application.config.secret_token = '176b3e55aa092b2bf7ba8d2ed39fd7df1a35bcd21ac655ee392d18c7b5064f32ee32cd6a6d19b6c17e3f3510f0d7277a84259d11400605f07466f1ac3387e980'
