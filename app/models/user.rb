f8987806ff61719e74a82f05ece50d372bca4212
Francisco Salinas <fcosalinas1987@gmail.com>
2013-01-03 16:28:03 -0300

    default isadmin

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
 
+  after_initialize :default_values
+
+
   validates :first_name,  presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -25,8 +28,13 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   validates_confirmation_of :password
-
+  
   def full_name
     return first_name + " " + last_name
   end
+  
+  private
+    def default_values
+      self.isadmin ||= false
+    end
 end
